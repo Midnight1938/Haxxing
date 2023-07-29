c0ec5115820b91ba8b87a7b19bc8e6a510fd2281
skshm <eminate894567@protonmail.com>
2023-07-29 18:19:58 +0530

    setup consistent container and files

--- launch.sh
+++ launch.sh
@@ -1,3 +1,6 @@
-podman build -t kali-box .
-podman run -p 5900:5900 --rm -i -t -v ~/Documents/Dev/Haxxing/container:/root/data/ kali-box
+#!/bin/bash
+#podman build -t kali-box .
+## podman run -p 5900:5900 -i -t -v ~/Documents/Dev/Haxxing/container:/root/data/ kali-box
+podman start b168bf0ff62e
+podman exec -i -t b168bf0ff62e /bin/bash
 notify-send "Ready" "VNC into localhost:5900\npwd: kallie"

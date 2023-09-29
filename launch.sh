a7187f935e15067b0a766209f1bdd49c8813b544
skshm <eminate894567@protonmail.com>
2023-09-29 23:46:35 +0530

    usual update for container name

--- launch.sh
+++ launch.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
-#podman build -t kali-box .
+# podman build -t kali-box .
+##Alt: podman pull quay.io/midnight1938/kali-box
 ## podman run -p 5900:5900 -i -t -v ~/Documents/Dev/Haxxing/container:/root/data/ kali-box
-podman start 1a37661e08a1
+podman start 2c9c12dbad81
 notify-send "Ready" "VNC into localhost:5900\npwd: kallie"
-podman exec -i -t 1a37661e08a1 /bin/bash
+podman exec -i -t 2c9c12dbad81 /bin/bash
 notify-send "Reminder" "Container is still active!"
